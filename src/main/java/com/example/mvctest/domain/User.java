325798f170ded0503ef881814159602ec6bf87dd
JoWoong <whdnd5725@gmail.com>
2025-09-11 14:16:17 +0900

    feat : basic package structure, temporary user repository

---
+++ src/main/java/com/example/mvctest/domain/User.java
@@ -0,0 +1,11 @@
+package com.example.mvctest.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private long id;
+    // ...
+}
---
+++ src/main/java/com/example/mvctest/repository/userrepository/UserMemoryRepository.java
@@ -0,0 +1,43 @@
+package com.example.mvctest.repository.userrepository;
+
+import com.example.mvctest.domain.User;
+import lombok.Getter;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class UserMemoryRepository implements UserRepository{
+
+    private static Map<Long, User> store = new ConcurrentHashMap<>();
+    private static long sequence = 0L;
+
+    @Getter
+    private static final UserMemoryRepository instance = new UserMemoryRepository();
+
+    private UserMemoryRepository(){} // 이거 왜 하는거지?
+
+    @Override
+    public void save(User user) {
+        user.setId(++sequence);
+        store.put(user.getId(), user);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return store.get(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+}
